6bc4e6d819cfeb8737bcd1c1599e78ae2d8c94e8
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-07-09 22:41:41 +0600

    signin validation create

--- src/app/module/user/user.validation.ts
+++ src/app/module/user/user.validation.ts
@@ -1,18 +1,30 @@
 import { z } from 'zod';
 
-export const UserSchemaZod = z.object({
-    name: z.string({
-        required_error: "Name is required",
-        invalid_type_error: "Name must be a string",
-    }),
-    email: z.string().email("Invalid email address"),
-    role: z.enum(['user', 'admin']),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
-    phone: z.string({
-        required_error: "Phone number is required",
-    }),
-    address: z.string({
-        required_error: "Address is required",
-        invalid_type_error: "Address must be a string",
-    }),
+const UserSignupSchemaZod = z.object({
+    body: z.object({
+        name: z.string({
+            required_error: "Name is required",
+            invalid_type_error: "Name must be a string",
+        }),
+        email: z.string().email("Invalid email address"),
+        role: z.enum(['user', 'admin']),
+        password: z.string().min(6, "Password must be at least 6 characters long"),
+        phone: z.string({
+            required_error: "Phone number is required",
+        }),
+        address: z.string({
+            required_error: "Address is required",
+            invalid_type_error: "Address must be a string",
+        }),
+    })
+
 });
+const UserSigninSchemaZod = z.object({
+    body: z.object({
+        email: z.string().email("Invalid email address"),
+        password: z.string().min(6, "Password must be at least 6 characters long"),
+    })
+
+});
+
+export { UserSignupSchemaZod, UserSigninSchemaZod }
