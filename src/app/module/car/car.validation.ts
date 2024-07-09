28ba1636786dbb202e58cef3bdbac6741da98dd8
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-07-09 21:56:06 +0600

    zod validation create

---
+++ src/app/module/car/car.validation.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const CarSchemaZod = z.object({
+    name: z.string({
+        required_error: "Name is required",
+        invalid_type_error: "Name must be a string",
+    }),
+    description: z.string().optional(),
+    color: z.string({
+        required_error: "Color is required",
+        invalid_type_error: "Color must be a string",
+    }),
+    isElectric: z.boolean(),
+    status: z.enum(['available', 'unavailable']).default('available'),
+    features: z.array(z.string()).optional(),
+    pricePerHour: z.number().positive("Price per hour must be a positive number"),
+    isDeleted: z.boolean().default(false),
+});
--- src/app/module/user/user.validation.ts
+++ src/app/module/user/user.validation.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 
-// Define the Zod schema for user validation
 export const UserSchemaZod = z.object({
     name: z.string({
         required_error: "Name is required",
