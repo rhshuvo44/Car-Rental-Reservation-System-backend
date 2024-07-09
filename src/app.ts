660f2b04ce9909d5945b7ad6841fd2768adfd37a
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-07-09 23:12:58 +0600

    not found route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import router from './app/route';
+import cookieParser from 'cookie-parser';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
+
+
 // application routes
 app.use('/api', router);
 
@@ -12,11 +17,6 @@ app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
     res.send('Assignment 3 Car Rental Reservation System Backend Server');
 });
-// Not Found Route Handler
-app.use((req: Request, res: Response) => {
-    res.status(404).json({
-        success: false,
-        message: 'Route not found',
-    });
-});
+//Not Found
+app.use(notFound);
 export default app;
