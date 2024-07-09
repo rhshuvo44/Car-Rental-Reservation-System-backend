10b4820cd7bc65be62b5256bc69596d781f270e7
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-07-09 23:16:11 +0600

    high order function create

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
+const notFound = (req: Request, res: Response,) => {
     return res.status(httpStatus.NOT_FOUND).json({
         success: false,
         message: 'API Not Found !!',
