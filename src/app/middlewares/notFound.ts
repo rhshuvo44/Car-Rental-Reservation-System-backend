1f63a4d47eab9a205d9a3045feff6b0a6be4b28e
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-01 16:39:45 +0600

    not found route strucher change

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -4,8 +4,9 @@ import httpStatus from 'http-status'
 const notFound = (req: Request, res: Response) => {
   return res.status(httpStatus.NOT_FOUND).json({
     success: false,
+    "statusCode": httpStatus.NOT_FOUND,
     message: 'API Not Found !!',
-    error: '',
+
   })
 }
 
