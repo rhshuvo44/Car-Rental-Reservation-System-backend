9d9034d23ceb95b6af93028c9ae31529c29cac49
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-02 12:24:33 +0600

    all api auth

--- src/app/module/booking/booking.rotes.ts
+++ src/app/module/booking/booking.rotes.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { bookingService } from './booking.service'
+import { USER_ROLE } from '../user/user.constant'
+import auth from '../../middlewares/auth'
 const router = express.Router()
 
-router.post('/', bookingService.createBooking)
-router.get('/', bookingService.getAllBooking)
-router.get('/my-bookings', bookingService.getMyBookings)
+router.post('/', auth(USER_ROLE.user), bookingService.createBooking)
+router.get('/', auth(USER_ROLE.admin), bookingService.getAllBooking)
+router.get('/my-bookings', auth(USER_ROLE.user), bookingService.getMyBookings)
 
 export const bookingRouter = router
--- src/app/module/car/car.rotes.ts
+++ src/app/module/car/car.rotes.ts
@@ -1,12 +1,34 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import auth from '../../middlewares/auth'
+import validateRequest from '../../middlewares/validateRequest'
+import { USER_ROLE } from '../user/user.constant'
 import { carController } from './car.controller'
+import { CarSchemaZod } from './car.validation'
 const router = express.Router()
 
-router.post('/', carController.createCar)
+router.post(
+  '/',
+  auth(USER_ROLE.admin),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data)
+    next()
+  },
+  validateRequest(CarSchemaZod),
+  carController.createCar,
+)
 router.get('/', carController.getAllCar)
 router.get('/:id', carController.getACar)
-router.put('/:id', carController.updateCar)
-router.delete('/:id', carController.deletedCar) //soft deleted from
-router.put('/return', carController.returnCar)
+router.put(
+  '/:id',
+  auth(USER_ROLE.admin),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data)
+    next()
+  },
+  // validateRequest(CarSchemaZod),
+  carController.updateCar,
+)
+router.delete('/:id', auth(USER_ROLE.admin), carController.deletedCar) //soft deleted from
+router.put('/return', auth(USER_ROLE.admin), carController.returnCar)
 
 export const carRouter = router
